4c6b8285339f41e014cbd4308fdf6cea1eeb0f7b
John Chamver Puno <tchamver@hotmail.com>
2026-02-15 20:28:09 +0800

    chore: remove linea and linea sepolia networks

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,18 +1,16 @@
 import { createConfig, http, cookieStorage, createStorage } from "wagmi";
-import { lineaSepolia, linea, mainnet } from "wagmi/chains";
+import { mainnet } from "wagmi/chains";
 import { metaMask } from "wagmi/connectors";
 
 export function getConfig() {
   return createConfig({
-    chains: [lineaSepolia, linea, mainnet],
+    chains: [mainnet],
     connectors: [metaMask()],
     ssr: true,
     storage: createStorage({
       storage: cookieStorage,
     }),
     transports: {
-      [lineaSepolia.id]: http(),
-      [linea.id]: http(),
       [mainnet.id]: http(),
     },
   });
