69cafd6c0aef3309b46cc7470de84e8b2c071414
John Chamver Puno <tchamver@hotmail.com>
2026-02-21 03:19:55 +0800

    refactor: overhaul design of the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { headers } from "next/headers";
 import { cookieToInitialState } from "wagmi";
-import "./globals.css";
+
 import { getConfig } from "@/wagmi.config";
+
+import "./globals.css";
 import { Providers } from "./providers";
-import { Navbar } from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,10 +39,9 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-black bg-opacity-90 text-foreground antialiased`}
       >
-        <div className="fixed inset-0 w-full h-full bg-repeat bg-noise opacity-25 bg-[length:350px] z-[-20] before:content-[''] before:absolute before:w-[2500px] before:h-[2500px] before:rounded-full before:blur-[100px] before:-left-[1000px] before:-top-[2000px] before:bg-white before:opacity-50 before:z-[-100]"></div>
-        <main className="flex flex-col max-w-screen-lg mx-auto pb-20">
+        <div className="fixed inset-0 w-full bg-repeat bg-noise opacity-25 bg-[length:350px] z-[-20]" />
+        <main className="flex flex-col max-w-screen-lg mx-auto">
           <Providers initialState={initialState}>
-            <Navbar />
             {children}
           </Providers>
         </main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,134 @@
-export const runtime = "edge";
+"use client"
 
-import { Separator } from "@/components/ui/separator";
-import { Hero } from "@/components/Hero";
+import Image from "next/image"
+import { useEffect, useState } from "react"
+import { useAccount, useBalance, useConnect, useDisconnect } from "wagmi"
+import clsx from "clsx"
+
+import { Button } from "@/components/ui/button"
+import { HoverEffect } from "@/components/ui/card-hover-effect"
+import { formatAddress } from "@/lib/utils"
+import { Separator } from "@/components/ui/separator"
+
+type Balance = {
+  title: string
+  description: string
+  link: string
+}
 
 export default function Home() {
+  const { address, isConnected, isConnecting, isReconnecting } = useAccount()
+  const { connect, connectors } = useConnect()
+  const { disconnect } = useDisconnect()
+  const { data: ethBalance, error: ethError } = useBalance({
+    address
+  })
+  const { data: usdtBalance, error: usdtError } = useBalance({
+    address,
+    token: "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+  })
+
+  const connector = connectors[0]
+
+  const [balances, setBalances] = useState<Balance[]>([
+    {
+      title: "Address",
+      description: "-",
+      link: "#1",
+    },
+    {
+      title: "ETH",
+      description: "-",
+      link: "#2"
+    },
+    {
+      title: "USDT",
+      description: "-",
+      link: "#3"
+    },
+  ])
+
+  const formatBigInt = (number: bigint, decimals: number) => Number(number) / 10 ** decimals
+
+  useEffect(() => {
+    console.log(ethBalance)
+    if (ethBalance?.value && usdtBalance?.symbol) {
+      setBalances([
+        {
+          title: "Address",
+          description: formatAddress(address),
+          link: "#1",
+        },
+        {
+          title: "ETH",
+          description: formatBigInt(ethBalance.value, ethBalance.decimals).toFixed(12),
+          link: "#2"
+        },
+        {
+          title: "USDT",
+          description: formatBigInt(usdtBalance.value, usdtBalance.decimals).toFixed(2),
+          link: "#3"
+        },
+      ])
+    }
+  }, [ethBalance, usdtBalance, isConnected])
+
+  const onConnectWallet = () => connect({ connector })
+  const onDisconnectWallet = () => disconnect()
+
   return (
-    <main className="">
-      <div className="flex flex-col gap-8 items-center sm:items-start w-full px-3 md:px-0">
-        <Hero />
-
-        <Separator className="w-full my-14 opacity-15" />
-
-        <footer className="flex flex-col items-center justify-center w-full">
-        </footer>
+    <>
+      <section className="flex flex-col h-full md:flex-row w-full space-y-3 py-10 px-3 md:px-6 ht-fit justify-center items-center">
+        <div>
+          {
+            <Button
+              className={clsx(
+                "shadow-xl md:px-10 font-semibold",
+                isConnected ? "bg-red-800 hover:bg-red-900" : "bg-blue-800 hover:bg-blue-900"
+              )}
+              onClick={isConnected ? onDisconnectWallet : onConnectWallet}
+            >
+              {isConnected ? "Disconnect Wallet" : "Connect Wallet"}
+            </Button>
+          }
+        </div>
+      </section>
+      
+      <div className="max-w-5xl mx-auto px-8">
+        <HoverEffect items={balances} />
       </div>
-    </main>
-  );
+
+      <Separator className="w-full mt-14 mb-4 opacity-15" />
+
+      <footer className="w-full border-t border-zinc-800 mt-8">
+        <div className="max-w-5xl mx-auto px-6 py-8 grid grid-cols-1 sm:grid-cols-2 gap-8 items-start">
+
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-2 text-white/80 font-semibold tracking-widest uppercase text-sm font-mono">
+              <Image
+                src={isConnected ? "/green-circle.svg" : "/circle.svg"}
+                alt="Circle"
+                width={8}
+                height={8}
+              />
+              <div className="text-[10px] align-middle">
+                {isConnected ? "Connected" : "Disconnected"}
+              </div>
+            </div>
+            <span className="text-zinc-600 text-[10px] tracking-wide font-mono">
+              © {new Date().getFullYear()} — All rights reserved
+            </span>
+          </div>
+
+          <p className="text-zinc-500 tracking-wide leading-relaxed text-[10px]">
+            This website is not affiliated with or endorsed by GetBlock.io, MetaMask, Wagmi or any company.
+            This is only for demonstration purposes and a personal project the website connects to the wallet
+            MetaMask if allowed. Built using NextJS, React, Wagmi, MetaMask SDK, TypeScript, TailwindCSS and
+            deployed to Cloudflare.
+          </p>
+
+        </div>
+      </footer>
+    </>
+  )
 }
